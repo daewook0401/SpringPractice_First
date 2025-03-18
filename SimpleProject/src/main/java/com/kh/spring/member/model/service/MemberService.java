f389c4c26c7a7a2745467d73e0267168c8bbc74e
daewook0401 <daewook0401@naver.com>
2025-03-18 17:47:01 +0900

    첫 spring 세팅 완

---
+++ SimpleProject/src/main/java/com/kh/spring/member/model/service/MemberService.java
@@ -0,0 +1,32 @@
+package com.kh.spring.member.model.service;
+
+import com.kh.spring.member.model.dto.MemberDTO;
+
+public interface MemberService {
+	
+	// 로그인
+	MemberDTO login(MemberDTO member);
+	
+	// 회원가입 
+	// 좋은방법 : 가입된 회원의 정보를 반환해준다. (Hibernate)
+	// 일반적인방법 : 정수값을 반환하거나 
+	//			   값을 반환하지 않는다. (MyBatis)
+	MemberDTO signUp(MemberDTO member);
+	
+	// 회원정보수정 
+	MemberDTO update(MemberDTO member);
+	
+	// 회원 탈퇴 
+	int delete(MemberDTO member);
+	
+	// 1절 끝
+	
+	
+	// 아이디 중복체크 
+	
+	// 2절 
+	
+	// 이메일인증 
+	
+	
+}
